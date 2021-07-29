2535a8109f6298e526f228d7063d44b46b185bc7
Edwin Vera <edwinvera@edwins-air.myfiosgateway.com>
2021-07-28 22:20:35 -0400

    Change the array of weight and products

--- app.js
+++ app.js
@@ -1,168 +1,275 @@
 //javascript
-
-const dosage = {
-  xxSmall: {
-    weight: "1-5lbs",
+const dosageArr = [
+  {
+    weight: "1-5",
     oil: {
-      regularStrength: {
-        startingDose: "1 drop (twice daily)",
-        maxDose: "3 drops (twice daily)",
-      },
-      extraStrength: {
-        startingDose: "Not Recommended",
-        maxDose: "Not Recommended"
-      }
+      regularStarting: "1 drop (twice daily)",
+      regularMax: "3 drops (twice daily)",
+      extraStarting: "Not Recommended",
+      extraMax: "Not Recommended"
     },
     chews: {
-      chew3mg: {
-        startingDose: "1/2 chew (once daily)",
-        maxDose: "1 chew (twice daily)"
-      },
-      chew6mg: {
-        startingDose: "Not Recommended",
-        maxDose: "Not Recommended"
-      }
+      chew3mgStarting: "1/2 chew (once daily)",
+      chew3mgMax: "1 chew (twice daily)",
+      chew6mgStarting: "Not Recommended",
+      chew6mgMax: "Not Recommended"
     }
   },
-  xSmall: {
-      weight: "6-10lbs",
+  {
+    weight: "6-10",
     oil: {
-      regularStrength: {
-        startingDose: "2 drops (twice daily)",
-        maxDose: "1/4mL (twice daily)",
-      },
-      extraStrength: {
-        startingDose: "Not Recommended",
-        maxDose: "Not Recommended"
-      }
+      regularStarting: "2 drops (twice daily)",
+      regularMax: "1/4mL (twice daily)",
+      extraStarting: "Not Recommended",
+      extraMax: "Not Recommended"
     },
     chews: {
-      chew3mg: {
-        startingDose: "Starting: 1/2 chew (twice daily)",
-        maxDose: "1 chew (twice daily)"
-      },
-      chew6mg: {
-        startingDose: "Not Recommended",
-        maxDose: "Not Recommended"
-      }
+      chew3mgStarting: "1/2 chew (twice daily)",
+      chew3mgMax: "1 chew (twice daily)",
+      chew6mgStarting: "Not Recommended",
+      chew6mgMax: "Not Recommended"
     }
   },
-  small: {
-      weight: "11-25lbs",
-      oil: {
-        regularStrength: {
-          startingDose: "4-6 drops (twice daily)",
-          maxDose: "1/2mL (twice daily)",
-        },
-        extraStrength: {
-          startingDose: "2-3 drops (twice daily)",
-          maxDose: "1/4ml (twice daily)",
-        }
-      },
-      chews: {
-          chew3mg: {
-              startingDose: "1 chew (once daily)",
-              maxDose: "1 chew (twice daily)"
-              },
-          chew6mg: {
-              startingDose: "1/2 chew (once daily)",
-              maxDose: "1/2 chew (twice daily)"
-          }
-      }
+  {
+    weight: "11-25",
+    oil: {
+      regularStarting: "4-6 drops (twice daily)",
+      regularMax: "1/2mL (twice daily)",
+      extraStarting: "2-3 drops (twice daily)",
+      extraMax: "1/4ml (twice daily)"
+    },
+    chews: {
+      chew3mgStarting: "1 chew (once daily)",
+      chew3mgMax: "1 chew (twice daily)",
+      chew6mgStarting: "1/2 chew (once daily)",
+      chew6mgMax: "1/2 chew (twice daily)"
+    }
   },
-  medium: {
-      weight: "26-50lbs",
-      oil: {
-        regularStrength: {
-          startingDose: "1/2ml (twice daily)",
-          maxDose: "1ml (twice daily)",
-        },
-        extraStrength: {
-          startingDose: "4-6 drops (twice daily)",
-          maxDose: "1/2ml (twice daily)",
-        }
-      },
-      chews: {
-          chew3mg: {
-              startingDose: "1 chew (twice daily)",
-              maxDose: "2 chews (twice daily)"
-              },
-          chew6mg: {
-              startingDose: "1/2 chew (twice daily)",
-              maxDose: "1 chew (twice daily)"
-          }
-      }
+  {
+    weight: "26-50",
+    oil: {
+      regularStarting: "1/2mL (twice daily)",
+      regularMax: "1mL (twice daily)",
+      extraStarting: "4-6 drops (twice daily)",
+      extraMax: "1/2mL (twice daily)"
+    },
+    chews: {
+      chew3mgStarting: "1 chew (twice daily)",
+      chew3mgMax: "2 chews (twice daily)",
+      chew6mgStarting: "1/2 chew (twice daily)",
+      chew6mgMax: "1 chew (twice daily)"
+    }
   },
-  large: {
-      weight: "51-75lbs",
-      oil: {
-        regularStrength: {
-          startingDose: "3/4ml (twice daily)",
-          maxDose: "1.5ml (twice daily)",
-        },
-        extraStrength: {
-          startingDose: "1/4ml (twice daily)",
-          maxDose: "1ml (twice daily)",
-        }
-      },
-      chews: {
-          chew3mg: {
-              startingDose: "1.5 chews (twice daily)",
-              maxDose: "2 chews (twice daily)"
-              },
-          chew6mg: {
-              startingDose: "1 chew (twice daily)",
-              maxDose: "1.5 chews (twice daily)"
-          }
-      }
+  {
+    weight: "51-75",
+    oil: {
+      regularStarting: "3/4mL (twice daily)",
+      regularMax: "1.5mL (twice daily)",
+      extraStarting: "1/4mL (twice daily)",
+      extraMax: "1mL (twice daily)"
+    },
+    chews: {
+      chew3mgStarting: "1.5 chews (twice daily)",
+      chew3mgMax: "2 chews (twice daily)",
+      chew6mgStarting: "1 chew (twice daily)",
+      chew6mgMax: "1.5 chews (twice daily)"
+    }
   },
-  xLarge: {
-      weight: "76-100lbs",
-      oil: {
-        regularStrength: {
-          startingDose: "1ml (twice daily)",
-          maxDose: "2ml (twice daily)",
-        },
-        extraStrength: {
-          startingDose: "1/2ml (twice daily)",
-          maxDose: "1.5ml (twice daily)",
-        }
-      },
-      chews: {
-          chew3mg: {
-              startingDose: "2 chews (twice daily)",
-              maxDose: "3 chews (twice daily)"
-              },
-          chew6mg: {
-              startingDose: "1.5 chews (twice daily)",
-              maxDose: "2 chews (twice daily)"
-          }
-      }
+  {
+    weight: "76-100",
+    oil: {
+      regularStarting: "1mL (twice daily)",
+      regularMax: "2mL (twice daily)",
+      extraStarting: "1/2mL (twice daily)",
+      extraMax: "1.5mL (twice daily)"
+    },
+    chews: {
+      chew3mgStarting: "2 chews (twice daily)",
+      chew3mgMax: "3 chews (twice daily)",
+      chew6mgStarting: "1.5 chews (twice daily)",
+      chew6mgMax: "2 chews (twice daily)"
+    }
   },
-  xxLarge: {
-      weight: "100+lbs",
-      oil: {
-        regularStrength: {
-          startingDose: "1.5ml (twice daily)",
-          maxDose: "2.5ml (twice daily)",
-        },
-        extraStrength: {
-          startingDose: "1ml (twice daily)",
-          maxDose: "2ml (twice daily)",
-          }
-      },
-      chews: {
-          chew3mg: {
-              startingDose: "3 chews (twice daily)",
-              maxDose: "4 chews (twice daily)"
-          },
-          chew6mg: {
-              startingDose: "2 chews (twice daily)",
-              maxDose: "3 chews (twice daily)"
-          }
-      }
+  {
+    weight: "100",
+    oil: {
+      regularStarting: "1.5mL (twice daily)",
+      regularMax: "2.5mL (twice daily)",
+      extraStarting: "1mL (twice daily)",
+      extraMax: "2mL (twice daily)"
+    },
+    chews: {
+      chew3mgStarting: "3 chews (twice daily)",
+      chew3mgMax: "4 chews (twice daily)",
+      chew6mgStarting: "2 chews (twice daily)",
+      chew6mgMax: "3 chews (twice daily)"
+    }
   }
-}
+]
+
+// const dosage = {
+//   xxSmall: {
+//     weight: "1-5",
+//     oil: {
+//       regularStrength: {
+//         startingDose: "1 drop (twice daily)",
+//         maxDose: "3 drops (twice daily)",
+//       },
+//       extraStrength: {
+//         startingDose: "Not Recommended",
+//         maxDose: "Not Recommended"
+//       }
+//     },
+//     chews: {
+//       chew3mg: {
+//         startingDose: "1/2 chew (once daily)",
+//         maxDose: "1 chew (twice daily)"
+//       },
+//       chew6mg: {
+//         startingDose: "Not Recommended",
+//         maxDose: "Not Recommended"
+//       }
+//     }
+//   },
+//   xSmall: {
+//       weight: "6-10",
+//     oil: {
+//       regularStrength: {
+//         startingDose: "2 drops (twice daily)",
+//         maxDose: "1/4mL (twice daily)",
+//       },
+//       extraStrength: {
+//         startingDose: "Not Recommended",
+//         maxDose: "Not Recommended"
+//       }
+//     },
+//     chews: {
+//       chew3mg: {
+//         startingDose: "1/2 chew (twice daily)",
+//         maxDose: "1 chew (twice daily)"
+//       },
+//       chew6mg: {
+//         startingDose: "Not Recommended",
+//         maxDose: "Not Recommended"
+//       }
+//     }
+//   },
+//   small: {
+//       weight: "11-25",
+//       oil: {
+//         regularStrength: {
+//           startingDose: "4-6 drops (twice daily)",
+//           maxDose: "1/2mL (twice daily)",
+//         },
+//         extraStrength: {
+//           startingDose: "2-3 drops (twice daily)",
+//           maxDose: "1/4ml (twice daily)",
+//         }
+//       },
+//       chews: {
+//           chew3mg: {
+//               startingDose: "1 chew (once daily)",
+//               maxDose: "1 chew (twice daily)"
+//               },
+//           chew6mg: {
+//               startingDose: "1/2 chew (once daily)",
+//               maxDose: "1/2 chew (twice daily)"
+//           }
+//       }
+//   },
+//   medium: {
+//       weight: "26-50",
+//       oil: {
+//         regularStrength: {
+//           startingDose: "1/2ml (twice daily)",
+//           maxDose: "1ml (twice daily)",
+//         },
+//         extraStrength: {
+//           startingDose: "4-6 drops (twice daily)",
+//           maxDose: "1/2ml (twice daily)",
+//         }
+//       },
+//       chews: {
+//           chew3mg: {
+//               startingDose: "1 chew (twice daily)",
+//               maxDose: "2 chews (twice daily)"
+//               },
+//           chew6mg: {
+//               startingDose: "1/2 chew (twice daily)",
+//               maxDose: "1 chew (twice daily)"
+//           }
+//       }
+//   },
+//   large: {
+//       weight: "51-75",
+//       oil: {
+//         regularStrength: {
+//           startingDose: "3/4ml (twice daily)",
+//           maxDose: "1.5ml (twice daily)",
+//         },
+//         extraStrength: {
+//           startingDose: "1/4ml (twice daily)",
+//           maxDose: "1ml (twice daily)",
+//         }
+//       },
+//       chews: {
+//           chew3mg: {
+//               startingDose: "1.5 chews (twice daily)",
+//               maxDose: "2 chews (twice daily)"
+//               },
+//           chew6mg: {
+//               startingDose: "1 chew (twice daily)",
+//               maxDose: "1.5 chews (twice daily)"
+//           }
+//       }
+//   },
+//   xLarge: {
+//       weight: "76-100",
+//       oil: {
+//         regularStrength: {
+//           startingDose: "1ml (twice daily)",
+//           maxDose: "2ml (twice daily)",
+//         },
+//         extraStrength: {
+//           startingDose: "1/2ml (twice daily)",
+//           maxDose: "1.5ml (twice daily)",
+//         }
+//       },
+//       chews: {
+//           chew3mg: {
+//               startingDose: "2 chews (twice daily)",
+//               maxDose: "3 chews (twice daily)"
+//               },
+//           chew6mg: {
+//               startingDose: "1.5 chews (twice daily)",
+//               maxDose: "2 chews (twice daily)"
+//           }
+//       }
+//   },
+//   xxLarge: {
+//       weight: "100",
+//       oil: {
+//         regularStrength: {
+//           startingDose: "1.5ml (twice daily)",
+//           maxDose: "2.5ml (twice daily)",
+//         },
+//         extraStrength: {
+//           startingDose: "1ml (twice daily)",
+//           maxDose: "2ml (twice daily)",
+//           }
+//       },
+//       chews: {
+//           chew3mg: {
+//               startingDose: "3 chews (twice daily)",
+//               maxDose: "4 chews (twice daily)"
+//           },
+//           chew6mg: {
+//               startingDose: "2 chews (twice daily)",
+//               maxDose: "3 chews (twice daily)"
+//           }
+//       }
+//   }
+// }
 
 const button = document.getElementById("btn")
 
@@ -172,7 +279,12 @@ const selectProduct = document.getElementById("select-product")
 button.addEventListener('click', function () {
   const valueWeight = selectWeight.options[selectWeight.selectedIndex].value
   const valueProduct = selectProduct.options[selectProduct.selectedIndex].value
-  
-  console.log(valueWeight)
-  console.log(valueProduct)
+
+  for (let i = 0; i < dosageArr.length; i++) {
+
+    // if (dosageArr[i] === valueProduct) {
+    //   console.log(dosageArr[i])
+    // }
+    console.log(dosageArr[i])
+  }
 })
